9a58e1db7147ae5ea731e085d6aef84116ef29c6
Tony McGee <tmcgee.au@gmail.com>
2022-12-03 23:05:10 +1000

    Day 3 part 2 completed

--- src/advent03/solution.spec.ts
+++ src/advent03/solution.spec.ts
@@ -1,26 +1,36 @@
 
-import solution, { Rucksack } from './solution';
+import solution, { ElfRucksack, ElfRucksackGroup } from './solution';
 
 describe(`Advent of Code Day ${solution.dayNumber}`, () => {
 
   it('should read input', () => {
-    var r = new Rucksack('vJrwpWtwJgWrhcsFMMfFFhFp');
+    var r = new ElfRucksack('vJrwpWtwJgWrhcsFMMfFFhFp');
     expect(r.comp1).toBe('vJrwpWtwJgWr');
     expect(r.comp2).toBe('hcsFMMfFFhFp');
     expect(r.shared).toBe('p');
     expect(r.priority).toBe(16);
-    var r = new Rucksack('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL');
+    var r = new ElfRucksack('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL');
     expect(r.comp1).toBe('jqHRNqRjqzjGDLGL');
     expect(r.comp2).toBe('rsFMfFZSrLrFZsSL');
     expect(r.shared).toBe('L');
     expect(r.priority).toBe(38);
   });
 
+  it('should find badge groups', () => {
+    var rg1 = new ElfRucksackGroup('vJrwpWtwJgWrhcsFMMfFFhFp', 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL' ,'PmmdzqPrVvPwwTWBwg')
+    expect(rg1.badge).toBe('r');
+    expect(rg1.priority).toBe(18);
+
+    var rg2 = new ElfRucksackGroup('wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn', 'ttgJtRGJQctTZtZT' ,'CrZsJsPPZsGzwwsLwLmpwMDw')
+    expect(rg2.badge).toBe('Z');
+    expect(rg2.priority).toBe(52);
+  })
+
   it('should solve part 1', () => {
     expect(solution.solvePart1()).toBe('8515');
   });
 
   it('should solve part 2', () => {
-    expect(solution.solvePart2()).toBe('xx');
+    expect(solution.solvePart2()).toBe('2434');
   });
 });
--- src/advent03/solution.ts
+++ src/advent03/solution.ts
@@ -1,7 +1,8 @@
 
 import { ISolution, InputFile } from '../shared';
 
-export class Rucksack {
+export class ElfRucksack {
+   content: string = '';
    comp1: string = '';
    comp2: string = '';
    shared: string = '';
@@ -9,14 +10,32 @@ export class Rucksack {
 
    constructor(input: string) {
       let len: number = input.length / 2;
+      this.content = input;
       this.comp1 = input.substring(0, len);
       this.comp2 = input.substring(len, input.length)
       this.shared = [...this.comp1].filter(x => this.comp2.includes(x))[0];
+      this.priority = ElfRucksack.calculatePriority(this.shared);
+   }
 
-      let code = this.shared.charCodeAt(0);
+   static calculatePriority(s: string) {
+      let code = s.charCodeAt(0);
       let codeLower = 'a'.charCodeAt(0);
       let codeUpper = 'A'.charCodeAt(0);
-      this.priority = code >= codeLower ? code - codeLower + 1 : code - codeUpper + 27;
+      return code >= codeLower ? code - codeLower + 1 : code - codeUpper + 27;
+   }
+}
+
+export class ElfRucksackGroup {
+   badge: string = '';
+   priority: number = 0;
+
+   constructor(s1: string, s2: string, s3: string) {
+      let rs1 = new ElfRucksack(s1);
+      let rs2 = new ElfRucksack(s2);
+      let rs3 = new ElfRucksack(s3);
+
+      this.badge = [...rs1.content].filter(x => rs2.content.includes(x) && rs3.content.includes(x))[0];
+      this.priority = ElfRucksack.calculatePriority(this.badge);
    }
 }
 
@@ -25,17 +44,21 @@ class Solution03 implements ISolution {
 
    solvePart1(): string {
       const inputFile = new InputFile(this.dayNumber);
-
-      let rs: Rucksack[] = inputFile.readLines().map(x => new Rucksack(x));
+      let rs: ElfRucksack[] = inputFile.readLines().map(x => new ElfRucksack(x));
 
       return '' + rs.map(r => r.priority).reduce((p, c) => p + c, 0);
    }
 
    solvePart2(): string {
       const inputFile = new InputFile(this.dayNumber);
-      //let test = new TestClass(inputFile.readLines());
+      let lines: string[] = inputFile.readLines();
 
-      return '';
+      let rsg: ElfRucksackGroup[] = lines
+         .map((x, i) => (i % 3) === 0 ? i : -1)
+         .filter(x => x >= 0)
+         .map(i => new ElfRucksackGroup(lines[i], lines[i + 1], lines[i + 2]));
+
+      return '' + rsg.map(r => r.priority).reduce((p, c) => p + c, 0);
    }
 }
 
